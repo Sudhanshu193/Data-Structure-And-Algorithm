51dd38f94f0561e75e707593a8a42841ef42f69f
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-04-04 21:37:51 +0530

    Template class

--- Essential C & C++ Concept/Template-classs.cpp
+++ Essential C & C++ Concept/Template-classs.cpp
@@ -7,37 +7,37 @@ private:
     T a;
     T b;
 public:
-    Arthmatic(T a , T b);
-    T add();
-    T sub();
-};
+        Arthmatic(T a , T b);
+        T add();
+        T sub();
+    };
 
-template<class T>
-Arthmatic<T>::Arthmatic(T a, T b)
-{
-  this->a=a;
-  this->b=b;
-}
+    template<class T>
+    Arthmatic<T>::Arthmatic(T a, T b) 
+    {
+    this->a=a;
+    this->b=b;
+    }
 
-template<class T>
-T Arthmatic<T>::add()
-{
+    template<class T>
+    T Arthmatic<T>::add()
+    {
+        T c;
+        c = a + b;
+        return c; 
+    }template<class T>
+    T Arthmatic<T>::sub()
+    {
     T c;
-    c = a + b;
-    return c; 
-}template<class T>
-T Arthmatic<T>::sub()
-{
- T c;
- c = a - b;
- return c;
-}
- 
- int main()
- {
-     Arthmatic<int> a(10,5);
-      cout << a.add()<<endl;
-      cout << a.sub()<<endl;
-      Arthmatic<float> a1(15.5, 8.25);
-      cout << a1.sub();
- }
+    c = a - b;
+    return c;
+    }
+    
+    int main()
+    {
+        Arthmatic<int> a(10,5);
+        cout << a.add()<<endl;
+        cout << a.sub()<<endl;
+        Arthmatic<float> a1(15.5, 8.25);
+        cout << a1.sub();
+    }
