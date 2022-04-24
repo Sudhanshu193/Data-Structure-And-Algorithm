3698217306225c13b151e9f18416cc7759097dfc
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-04-24 23:53:10 +0530

    Pattern

--- Questions/pattern.cpp
+++ Questions/pattern.cpp
@@ -5,6 +5,20 @@ int main()
   int i ,j,n;
   cout <<"Enter the number:"<<endl;
   cin >>n;
-  
-
+  for ( i = 0; i <= n; i++)
+  {
+      for ( j = 0; j <= n; j++)
+      {
+          if (i+j>=n)
+          {
+              cout <<"*";
+          }
+          else
+          {
+              cout <<" ";
+          }
+      }
+      cout << endl;
+  }
+   return 0;
 }
