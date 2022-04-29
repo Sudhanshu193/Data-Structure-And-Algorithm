a10db8deb3839c4edd5366c1ec79a8cf2d227ea4
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-04-29 23:28:19 +0530

    Pattern

---
+++ Questions/pattern2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <5 ; j++)
+        {
+            if (i+j>5)
+                cout <<"*";
+            else
+                cout <<" ";
+        }
+        for (int k =0; k <i; k++)
+        {
+            cout <<"*";
+        }
+        
+        cout<<endl;
+    }
+ 
+}
