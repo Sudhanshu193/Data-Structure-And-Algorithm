1dd6ecc4b2e0f05287063e3ce46ee43ea7f24a90
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-05-03 12:39:26 +0530

    Tree Recursion

---
+++ Recursion/Tree-Recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void fun(int n)
+{
+ if(n>0)
+ {  //when more then one recursive function is in the function then that function is know as tree recursion;
+     printf("%d", n);
+     fun(n-1);
+     fun(n-1);
+ }
+}
+int main()
+{
+int x =3;
+fun(3);
+
+return 0;
+}
