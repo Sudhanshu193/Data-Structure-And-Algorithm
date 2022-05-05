0e5371f77d43780ab930f24eb3f98ddde298e8c2
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-05-05 22:28:42 +0530

    Modified

--- Questions/Dublicate.cpp
+++ Questions/Dublicate.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-  int arr[5]={2,4,8,2,3};
+  int arr[6]={2,4,4,4,4,2};
   int count=0;
   int max= -1;
   int index=-1;
-  for(int i = 0; i < 5; i++)
+  for(int i = 0; i < 6; i++)
   {
-    for(int j = 0; j < 5; j++)
+    for(int j = 0; j < 6; j++)
     {
      if(arr[i]==arr[j])
      {
@@ -18,15 +18,13 @@ int main()
     if (count>max)
     {
        max = count;
-       index=i; 
+       index =i; 
     }
 
     count = 0;
         
   }
-  cout<< "Element: "<<max<<endl;
-  cout<< "Index:"<<index<<endl;
-
-  
-
+  cout << "Number of time: "<<max<<endl;
+  cout << "Element is: "<<arr[index]<<endl;
+  cout << "Index:"<<index<<endl;
 }
