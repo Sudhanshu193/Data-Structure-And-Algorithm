03258bc8eec1f4e3ccd9a396ef6008a12995c74a
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-05-08 22:18:22 +0530

    Modified

--- Questions/Binary Search/Question-1-Modified.cpp
+++ Questions/Binary Search/Question-1-Modified.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int binary_search(int arr[], int key)
+int binary_search1(int arr[], int key)
 {    int l =0;
     int h=9 ;
     int index =-1;
@@ -11,7 +11,7 @@ int binary_search(int arr[], int key)
         if(key == arr[mid])
         {
             index = mid;
-            h= mid -1;
+            h= mid - 1;
         }
         else if(key > arr[mid])
         {
@@ -21,18 +21,42 @@ int binary_search(int arr[], int key)
         {
             h = mid -1;
         }
-    
-
     }
     return mid;
+}
 
+int binary_search2(int arr[], int key)
+{
+    int l;
+    int h;
+    int mid;
+    int l_index;
+    while (l<=h)
+    {
+         mid= (l+h)/2;
 
+        if (key == arr[mid])
+        {
+            l_index = mid;
+            l = mid +1;
+        }
+        else if(key > arr[mid])
+        {
+            l = mid + 1;
+        }
+        else
+            h = mid - 1;
+        
+        
+    }
+    return mid;
 }
 int main()
 {
 int arr[10]={1,1,2,2,2,2,2,4,4,6};
 int key =2;
-int result = binary_search(arr , key);
-cout<<result;
+int result = binary_search1(arr , key);
+cout<<"First occurence at index: "<<result;
+cout<<" last occurence at index: "<<binary_search2(arr , key);
 
 }
