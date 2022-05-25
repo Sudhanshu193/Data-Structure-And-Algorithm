8eaf87513444e391c9ebd1e1160bd80aadf06158
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-05-26 02:45:00 +0530

    Array using class

--- Array ADT/Array-using-calss.cpp
+++ Array ADT/Array-using-calss.cpp
@@ -8,8 +8,39 @@ class array
     int size;
     int length;
 
+    void dispaly()
+    {
+        int i;
+        cout <<"The element are : ";
+        for ( i = 0; i < length; i++)
+        {
+          cout<<" "<<A[i];
+        }
+        
+    }
+
 };
 int main()
 {
+  array arr;
+  int n ,i;
+  cout <<"Enter the size of array:"<<endl;
+  cin >>arr.size;
+  arr.length=0;
+   
+    arr.A=new int[arr.size];
 
+  cout<<"Enter the number of element"<<endl;
+  cin >>n;
+  arr.length=n;
+  cout <<"Enter the elements:"<<endl;
+  for ( i = 0; i < n; i++)
+  {
+      cin >>arr.A[i];
+  }
+   
+ arr.dispaly();
+  
+
+  
 }
