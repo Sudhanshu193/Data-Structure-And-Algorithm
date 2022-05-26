785d9d860efdc5a3ca08ad5254e88b827e8f216a
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-05-26 15:13:11 +0530

    Operations in array

---
+++ Array ADT/Opration-on-array.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+
+struct array
+{
+    int A[10];
+    int size;
+    int length;
+};
+void display(struct array arr)
+{
+    int i;
+    for ( i = 0; i < arr.length; i++)
+    {
+        printf("%d ", arr.A[i]);
+    }
+    
+}
+
+void Insert(struct array *arr , int index , int x)
+{
+    int i;
+    if (index > 0 && index <arr->length)
+    {
+        for ( i = arr->length; i > index; i--)
+        {
+            arr->A[i]=arr->A[i-1];
+        }
+        arr->A[index]=x;
+        arr->length++;
+        
+    }
+    
+}
+void append(struct array *arr, int x)
+{
+    if(arr->length<arr->size)
+        {
+            arr->A[arr->length]=x;
+            arr->length++;
+        } 
+            
+}
+int Delete(struct array *arr, int index)
+{
+    int x=arr->A[index];
+    int i;
+    if (index >0 && index < arr->length)
+    {
+    for (i = index; i < arr->length; i++)
+    {
+        arr->A[i]=arr->A[i+1];
+    }
+    arr->length--;
+    return x;
+        
+    }
+    
+
+    
+
+}
+
+int main()
+{
+ struct array arr = {{1,2,3,4,5},10,5};
+   
+   //adding a element at the end
+    append(&arr,10);
+    display(arr);
+    printf("\n");
+    // inserting an element  at given index
+    Insert(&arr, 3 , 15);
+    display(arr);
+    printf("\n");
+    //deleting the element at given index
+   printf("%d\n", Delete(&arr, 2));
+    display(arr);
+
+}
