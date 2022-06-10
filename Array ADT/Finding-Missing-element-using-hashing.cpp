b100cd8b3b0b680b9a36d824dfb4a88aa2cf43f6
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-06-10 15:12:18 +0530

    Finding missing element using hasing

---
+++ Array ADT/Finding-Missing-element-using-hashing.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int A[10]={1,3,5,7,8,6,10,12,15,16};
+     int H[16]={0};
+     int low =1;
+     int high = 16;
+     int n =10;
+
+     for (int  i = 0; i < 10; i++)
+     {
+        H[A[i]]++;
+     }
+     for (int i = low; i < high; i++)
+     {
+        if (H[i]== 0)
+        {
+            cout <<i<<endl;
+        }
+     }
+     
+     
+}
