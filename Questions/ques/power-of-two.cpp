de8df39a514070039895c8cac19d896894b17849
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-06-12 06:31:57 +0530

    Power of two using log

---
+++ Questions/ques/power-of-two.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void value2(double d)
+{
+ float a = log2(d);
+ cout <<a<<endl;
+    if(a == (int) a)
+        cout <<"True";
+    else
+        cout <<"False";
+
+}
+int main()
+{ double n;
+    cout <<"enter input n"<<endl;
+    cin >>n;
+    value2(n);
+}
