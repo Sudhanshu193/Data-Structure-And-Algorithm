95d5118d5a99e53861de1ef642825819d8ff986e
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-09 13:45:09 +0530

    Linked List usng insert Function

---
+++ Linked Lists/LinkedListUsingInsert.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node {
+    int data;
+    struct Node* next;
+
+}*first=NULL;
+
+int count(struct Node *p)
+{
+    int N_count=0;
+    while (p!=NULL)
+    {
+        N_count++;
+        p=p->next;
+    }
+    return N_count;   
+}
+ 
+
+void insert(struct Node *p, int index , int x)
+{
+    int i;
+    struct Node *t;
+    if (index<0 ||index >count(p))
+    {
+        return;
+    }
+    
+    t=(struct Node*)malloc(sizeof(struct Node));
+    t->data=x;
+
+    if (index ==  0)
+    {
+        t->next=first;
+        first=t;
+    }
+    else
+    {
+        for ( i = 0; i < index-1; i++)
+        {
+            p=p->next;
+        }
+        t->next=p->next;
+        p->next=t;
+    }
+}
+void Display(struct Node *p)
+{
+    while (p!=NULL)
+    {
+       
+        printf("%d ", p->data);
+        p=p->next;;
+    }
+    
+}
+int main()
+{
+  insert(first, 0 ,10);
+  insert(first, 1,2);
+  insert(first, 0,20);
+  insert(first, 3,32);
+  Display(first);
+}
