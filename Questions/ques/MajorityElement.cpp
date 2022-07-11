d1e2279456ae3f964375afe27dd18f4fdea2838d
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-07-11 23:14:48 +0530

    Majority element

--- Questions/ques/MajorityElement.cpp
+++ Questions/ques/MajorityElement.cpp
@@ -4,21 +4,31 @@ int main()
 {
   int arr[]={2,2,1,1,1,2,2};
   int n = sizeof(arr)/sizeof(arr[0]);
-  //cout<<n/2;
+  
+  int count;
+  int maxCount=0;
+  int index=-1;
   for(int i=0; i<n; i++)
   {
-    int count =0;
+    count = 0;
     for (int j = 0; j < n; j++)
     {
         if(arr[i]==arr[j])
         {
             count++;
+
         }
     }
-    if(count > n/2)
+  if(maxCount<count)
+  {
+    maxCount=count;
+    index=i;
+  }
+    
+  }
+  if(count > n/2)
     {
-        cout<<" "<<arr[i];
+        cout<<" "<<arr[index];
 
     }
-  }
 }
