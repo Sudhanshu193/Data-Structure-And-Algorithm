1986c5478d6401a8cacc4c13eae7c0c4bdde2a0f
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-09-28 12:14:13 +0530

    BST implementation and searching

--- Trees/BSTimplementation.cpp
+++ Trees/BSTimplementation.cpp
@@ -28,7 +28,19 @@ struct BSTNode{
 
     return root;         
  } 
-   
+ bool search(BSTNode* root , int data)
+ {
+    if(root == nullptr)
+        return false;
+
+    if(data == root->data)
+        return true;
+    else if(data <= root->data)
+        return search(root->left, data);
+    else
+        return search(root->right, data);
+
+ }  
 int main()
 {
  BSTNode *root=nullptr;
@@ -41,10 +53,14 @@ int main()
  root = Insert(root, 27);
  root = Insert(root, 40);
 
- cout<<root->data;
+int number;
+cout<<"Enter the number: "<<endl;
+cin>>number;
 
-
- 
+if(search(root, number) == true)
+    cout<<"Number found";
+else
+    cout<<"Not found";    
 
 
 }
